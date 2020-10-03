877202ff4aa859bdfed4d63f37bc0d57df97d865
Taylor Shin <taylorshin12@gmail.com>
2020-10-03 09:22:22 -0700

    add scripts for experiments 11 and 12

---
+++ scripts/run_create_trigger_exp11.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Experiment 11
+# Task: fact retrieval (original)
+# Model: BERT
+# Batch sizes: 28
+# Iters: 1000
+# Filtering: True
+
+datadir=$1
+logfile=$2
+
+# Clear files
+cat /dev/null > $logfile
+cat /dev/null > ${logfile}.log
+
+for path in $datadir/*; do
+    filename=$(basename "$path")
+    CUDA_VISIBLE_DEVICES=0 python -m lmat.create_trigger \
+        --train $path/train.jsonl \
+        --dev $path/dev.jsonl \
+        --template '[CLS] {sub_label} [T] [T] [T] [T] [T] [T] [T] [P] . [SEP]' \
+        --num_cand 10 \
+        --accumulation-steps 1 \
+        --model-name bert-base-cased \
+        --bsz 28 \
+        --eval-size 28 \
+        --iters 1000 \
+        --label-field 'obj_label' \
+        --tokenize-labels \
+        --filter \
+        --print-lama >> $logfile 2>> ${logfile}.log
+done
