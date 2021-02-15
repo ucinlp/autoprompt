f1330d07c6a3129aa8658456fbb607cca4aaab8c
Robert Lockwood Logan IV <rloganiv@gmail.com>
2021-02-15 15:21:02 -0800

    Updated jobs + serialization bugfix

--- jobs/fugue/sh/multirc_cinf_2.sh
+++ jobs/fugue/sh/multirc_cinf_2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --job-name=cb_cinf
-#SBATCH --output=/extra/ucinlp0/rlogan/fugue_cb_cinf.txt
+#SBATCH --job-name=multirc_cinf_2
+#SBATCH --output=/extra/ucinlp0/rlogan/fugue_multirc_cinf_2.txt
 #SBATCH --time=1000:00
 #SBATCH --partition=ava_s.p
 #SBATCH --nodelist=ava-s0
@@ -10,5 +10,5 @@
 
 srun /home/rlogan/miniconda3/envs/autoprompt/bin/python3.7 \
     scripts/launch.py \
-    --logdir results/cb \
-    jobs/fugue/yaml/cb_cinf.yaml
+    --logdir results/multirc \
+    jobs/fugue/yaml/multirc_cinf_2.yaml
