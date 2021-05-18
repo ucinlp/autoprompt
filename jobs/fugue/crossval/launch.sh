9fee8a7c47c14bc1756d7ba21dfbdfbbe9567ed3
Robert Lockwood Logan IV <rloganiv@gmail.com>
2021-05-18 10:38:30 -0700

    Added configs

--- jobs/fugue/crossval/launch.sh
+++ jobs/fugue/crossval/launch.sh
@@ -2,8 +2,9 @@
 #SBATCH --time=100000:00
 #SBATCH --partition=ava_s.p
 #SBATCH --gpus=1
-#SBATCH --cpus-per-task=8
-#SBATCH --mem=50GB
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=30GB
+#SBATCH --exclude=ava-s4
 
 srun /home/rlogan/miniconda3/envs/autoprompt/bin/python3.7 \
     scripts/crossval.py \
